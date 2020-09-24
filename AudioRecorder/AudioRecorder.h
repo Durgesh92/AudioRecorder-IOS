ea5b21d3ed5b894f2c2c071c27d07fdafa3553a5
Durgesh92 <waghmaredurgesh@gmail.com>
2020-09-24 22:21:42 +0530

    first commit

---
+++ AudioRecorder/AudioRecorder.h
@@ -0,0 +1,19 @@
+//
+//  AudioRecorder.h
+//  AudioRecorder
+//
+//  Created by Durgesh Waghmare on 08/11/19.
+//  Copyright © 2019 Durgesh Waghmare. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for AudioRecorder.
+FOUNDATION_EXPORT double AudioRecorderVersionNumber;
+
+//! Project version string for AudioRecorder.
+FOUNDATION_EXPORT const unsigned char AudioRecorderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AudioRecorder/PublicHeader.h>
+
+
